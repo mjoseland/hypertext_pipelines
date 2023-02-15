ae043edc8738f92723a5fc3133abdcbc48a8d515
Mal Joseland <software@mjoseland.name>
2023-02-15 15:16:12 +1000

    added initial spring boot web project

---
+++ src/main/java/name/mjoseland/hypertextpipelines/HypertextPipelinesApplication.java
@@ -0,0 +1,13 @@
+package name.mjoseland.hypertextpipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypertextPipelinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HypertextPipelinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/name/mjoseland/hypertextpipelines/HypertextPipelinesApplicationTests.java
@@ -0,0 +1,14 @@
+package name.mjoseland.hypertextpipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypertextPipelinesApplicationTests {
+
+    @SuppressWarnings("java:S2699")
+    @Test
+    void contextLoads() {
+    }
+
+}
